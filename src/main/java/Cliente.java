3d13710b329060be7ab39c6954e6207caeb5ae81
Pedro Talma Toledo <pedrotoledo1717@gmail.com>
2023-11-16 15:35:26 -0300

    Início incompleto pq ta muito quente e eu não aguento mais ficar no PC suando

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String senha;
+    private ArrayList<Conta> contas = new ArrayList();
+
+}
---
+++ src/main/java/ContaRendaFixa.java
@@ -0,0 +1,2 @@
+public class ContaRendaFixa extends Conta{
+}
